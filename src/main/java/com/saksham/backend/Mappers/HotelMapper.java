88cd4399d30fa3e858d5c6112a1fc0df114bfc5a
Saksham Jain <sj20011002@gmail.com>
2023-05-23 15:18:56 +0530

    docker config

--- src/main/java/com/saksham/backend/Mappers/HotelMapper.java
+++ src/main/java/com/saksham/backend/Mappers/HotelMapper.java
@@ -3,8 +3,11 @@ package com.saksham.backend.Mappers;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.saksham.backend.Models.Hotel;
 
@@ -29,5 +32,10 @@ public interface HotelMapper {
     @Select("SELECT DISTINCT(h.id),h.*,min(price) as price from hotels{db} h,rooms r where h.state = #{state} and h.city = #{city} and r.`hotelId` = h.id and r.id not in (Select r.id from rooms as r,bookings as b,roombooked as rb WHERE r.id=rb.`roomID` and rb.`bookingId`=b.id and (b.`checkIn` BETWEEN CAST(#{checkIn} as Date) and #{checkOut} or b.`checkOut` BETWEEN CAST(#{checkIn} as Date) and #{checkOut}) and (`checkIn` is not null AND `checkOut` is not null)) GROUP BY(h.id) order by ${sort} LIMIT ${Limit}")
     List<Hotel> advanceSearch(String state, String city, LocalDate checkIn, LocalDate checkOut, int Limit, String sort,String db);
 
-    
+    @Insert("INSERT INTO hotels${db} () VALUES()")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    void addHotel(Hotel hotel,String db);
+
+    @Update("Update hotel set  where id = #{id}")
+    void editHotel();
 }
